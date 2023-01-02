2a42a8eff77c847ce1896b68ddcf88d9e74fe723
Stefan Viggiano <stefanviggiano@gmail.com>
2023-01-02 17:19:19 -0300

    Add CheckIdentityInterface

--- AdaCredit/AdaCredit.cs
+++ AdaCredit/AdaCredit.cs
@@ -7,24 +7,28 @@ using System.Threading.Tasks;
 
 namespace AdaCredit
 {
-
     public class AdaCreditApp
     {
-        private DatabaseClient client;
+        private DatabaseClient database_client;
 
-        public AdaCreditApp(DatabaseClient client)
+        public AdaCreditApp(DatabaseClient database_client)
         {
-            this.client = client;
+            this.database_client = database_client;
         }
 
         public static void Main()
         {
-            var client = new DatabaseClient();
-            var app = new AdaCreditApp(client);
-            app.login();
+            var database_client = new DatabaseClient();
+            var app = new AdaCreditApp(database_client);
+            app.Start();
         }
 
-        public void login()
+        public void Start()
+        {
+            this.Login();
+        }
+
+        public void Login()
         {
             Console.WriteLine("Type your login and password");
             Console.Write("Login: ");
@@ -32,12 +36,28 @@ namespace AdaCredit
             Console.Write("Password: ");
             string password = Console.ReadLine();
 
-            Console.WriteLine($"{login}, {password}");
+            List<Employee> employees = this.database_client.Employees;
 
-            this.login();
+            if (employees.Length == 0 && login == "user"
+                && password == "login")
+            {
+                this.CreatePassword();
+                return;
+            }
+
+            foreach (Employee employee in employees)
+            {
+                if (employee.CheckIdentity(login, password))
+                {
+                    this.MainScreen();
+                    return;
+                }
+            }
+
+            Console.WriteLine("Incorrect login and password");
+            this.Login();
+            }
         }
-
-    }
 
     public class Employee
     {
@@ -47,6 +67,13 @@ namespace AdaCredit
         public string PasswordSalt;
         public DateTime LastLogin;
         public bool Active;
+
+        public bool CheckIdentity(string login, string password)
+        {
+            if (login == this.Login && password == this.Password)
+                return true;
+            return false;
+        }
     }
 
 
@@ -55,6 +82,7 @@ namespace AdaCredit
         public string Name;
         public int AccountNumber;
         public int AgencyNumber;
+
     }
 
 
